379c043fc773664752b35437ef5218279c5bae0d
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-12 12:16:05 +0100

    BDD base done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,39 +1,49 @@
+-- create DATABASE anime_art;
+USE anime_art;
+
 create table user (
-  id int unsigned primary key auto_increment not null,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   firstname  VARCHAR(45) NOT NULL,
   lastname  VARCHAR(45) NOT NULL,
   email varchar(255) not null unique,
   password varchar(255) not null,
-  is_admin BOOLEAN NOT NULL DEFAULT FALSE,
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+
+create table season (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(45) NOT NULL
 );
 
 create table background (
-  id int unsigned primary key auto_increment not null,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   name  VARCHAR(45) NOT NULL,
   file VARCHAR(200) NOT NULL,
   season_id  INT NOT NULL,
-  source VARCHAR(200) NOT NULL,
   FOREIGN KEY (season_id) REFERENCES season(id)
 );
 
-create table season (
-  id int unsigned primary key auto_increment not null,
-  name  VARCHAR(45) NOT NULL,
-);
-
 create table favorite (
-  id int unsigned primary key auto_increment not null,
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   user_id INT NOT NULL,
   background_id INT NOT NULL,
   FOREIGN KEY (user_id) REFERENCES user(id),
   FOREIGN KEY (background_id) REFERENCES background(id)
 );
 
-insert into user(id, email, password)
+insert into user(firstname, lastname, email, password, is_admin)
 values
-  (1, "jdoe@mail.com", "123456");
+  ("Emilie", "De Duyver", "emilie.deduyver@gmail.com", "123456", 1 );
 
-insert into background(name, file, season_id, source)
+insert into season (name)
 values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+  ("Winter"),
+  ("Spring"),
+  ("Summer"),
+  ("Autumn");
+
+
+insert into background(name, file, season_id)
+values
+  ("Walking under rain", "/assets/images/autumn1.jpg", 4);
