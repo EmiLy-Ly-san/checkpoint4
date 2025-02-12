ab0b79477bb43180c2910c2c118090b7eff7eb10
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-12 18:37:18 +0100

    fetch backgrounds by season

---
+++ client/src/components/backgroundCard.tsx
@@ -0,0 +1,40 @@
+// function BackgroundCard({
+//   isFreemium,
+//   title,
+//   duration,
+//   id,
+//   thumbnailUrl,
+//   isLarge,
+//   displayCardInfo = true,
+// }: function BackgroundCard({
+// ) {
+
+//   return (
+//     <>
+//       <article className="video-card">
+//         <section className="card-content">
+//           <div className="video-item">
+//             <img
+//               src={background.file}
+//               width="300px"
+//               height="168px"
+//               alt={name}
+//               className="background-image"
+//             />
+//           </div>
+//         </section>
+//           <section className="card-footer">
+//             <div className="first-row-card-footer">
+//               <p className="background-title">{name}</p>
+//             </div>
+//                 <img
+//                   className="heart-icon"
+//                   src="/orange-heart.png"
+//                   alt="heart icon"
+//                 />
+//           </section>
+//       </article>
+//     </>
+//   );
+// }
+// export default BackgroundCard;
--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,108 @@
+import { useEffect, useState } from "react";
+import type { Background } from "../../types/Background";
+import useToast from "../../utils/useToastify";
+// import BackgroundCard from "../../components/backgroundCard";
+// import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  // const handleSeasonClick = () => {};
+  const [backgroundsSeason, setBackgroundsBySeason] = useState<Background[]>();
+  console.warn({ backgroundsSeason });
+  const [idOfTheSeason, setIdOfTheSeason] = useState<number>();
+  const { notifyError } = useToast();
+
+  const handleSeasonClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    if (event.currentTarget.dataset.id) {
+      const idTofind = Number.parseInt(`${event.currentTarget.dataset.id}`);
+      if (idTofind) {
+        setIdOfTheSeason(idTofind);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const urlForSeason = `${import.meta.env.VITE_API_URL}/api/backgrounds-season/${idOfTheSeason}`;
+    recoverInfoBackgrounds(urlForSeason);
+    // if (backgroundsSeason) {
+    //   const backgroundsToFind = backgroundsSeason.filter((backgrounds) => {
+    //     return backgrounds.season_id === idOfTheSeason;
+    //   });
+    //   console.log({ backgroundsToFind });
+
+    // navigate(`/video/${videoToFind?.id}`);
+    //   }
+    // }
+  }, [idOfTheSeason]);
+
+  async function recoverInfoBackgrounds(url: string) {
+    try {
+      const request = await fetch(url);
+      const datas = await request.json();
+
+      setBackgroundsBySeason(datas);
+    } catch (err) {
+      notifyError("You are log out !");
+    }
+  }
+
   return (
-    <section>
-      <button type="button">
-        {/* onClick={handleSeasonClick} */}
-        Winter backgrounds
-      </button>
-      <button type="button">Spring backgrounds</button>
-      <button type="button">Summer backgrounds</button>
-      <button type="button">Autumn backgrounds</button>
-    </section>
+    <>
+      <section>
+        <button
+          data-id="1"
+          type="button"
+          onClick={(event) => {
+            handleSeasonClick(event);
+          }}
+        >
+          Winter backgrounds
+        </button>
+        <button
+          data-id="2"
+          type="button"
+          onClick={(event) => {
+            handleSeasonClick(event);
+          }}
+        >
+          Spring backgrounds
+        </button>
+        <button
+          data-id="3"
+          type="button"
+          onClick={(event) => {
+            handleSeasonClick(event);
+          }}
+        >
+          Summer backgrounds
+        </button>
+        <button
+          data-id="4"
+          type="button"
+          onClick={(event) => {
+            handleSeasonClick(event);
+          }}
+        >
+          Autumn backgrounds
+        </button>
+      </section>
+      {/* <section>
+        {backgroundsSeason?.map((background) => (
+          <Link
+            to={`/background/${background.id}`}
+            className="carousel-slide"
+            key={background.id}
+          >
+            <BackgroundCard
+              title={background.name}
+              thumbnailUrl={`${import.meta.env.VITE_API_URL}${background.file}`}
+              isLarge
+              displayCardInfo={false}
+              id={background.id}
+            />
+          </Link>
+        ))}
+      </section> */}
+    </>
   );
 }
