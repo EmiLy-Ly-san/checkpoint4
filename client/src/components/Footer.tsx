482b1853dc94b4917f3fbeabe45be63bd1b8a0bf
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-13 14:45:47 +0100

    theme gestion

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,8 +1,10 @@
 import "../style/Footer.css";
+import useTheme from "../utils/useTheme";
 
 export default function Footer() {
+  const { theme } = useTheme();
   return (
-    <footer>
+    <footer className={`header-container ${theme ? "light" : "dark"}`}>
       <p>2024 copyright</p>
       <img src="/heart-purple.png" alt="" />
       <p>EmiLy-Ly-san</p>
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,26 @@
 import { NavLink } from "react-router-dom";
 import "../style/Header.css";
+import useTheme from "../utils/useTheme";
 
 export default function Header() {
+  const { theme, setTheme } = useTheme();
+  // console.log({ theme });
   return (
-    <header>
+    <header className={`header-container ${theme ? "light" : "dark"}`}>
       <NavLink className="logo" to={"/"}>
         Anime Art Season
       </NavLink>
+      <button
+        type="button"
+        onClick={() => setTheme(!theme)}
+        className="button-switch-theme"
+      >
+        <img
+          className="icon-switch-theme"
+          src={theme ? "/dark_mode.png" : "/light_mode.png"}
+          alt="switch theme icon"
+        />
+      </button>
       <nav>
         <NavLink to={"/admin"}>Admin</NavLink>
         <NavLink to={"/"}>Collections</NavLink>
