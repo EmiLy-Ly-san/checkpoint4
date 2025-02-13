98bfb5dbc0d3a204478472fa8869f61862196eaa
Emilie <150048527+EmiLy-Ly-san@users.noreply.github.com>
2025-02-13 09:25:43 +0100

    style admin basics + responsive header

--- client/src/pages/Admin/Admin.tsx
+++ client/src/pages/Admin/Admin.tsx
@@ -1,20 +1,24 @@
 import { NavLink, Outlet, useMatch } from "react-router-dom";
+import "../../style/HomePage.css";
 
 export default function Admin() {
   const match = useMatch("/admin"); // Match give the object routes in the parenthese if we are in this route during the navigation , otherwise, match returns null if it's not the case
 
   return (
-    <div className="admin-page">
+    <div className="home-page">
       <h1 className="title-admin">Admin section</h1>
       {!!match && (
-        <section className="button-admin-wrapper">
-          <NavLink className="admin-link" to={"./add-Background"}>
+        <section className="button-home-wrapper">
+          <NavLink to={"./add-Background"} className="standard-button">
             Add a background
           </NavLink>
-          <NavLink className="admin-link" to={"./update-delete-background"}>
+          <NavLink
+            className="standard-button"
+            to={"./update-delete-background"}
+          >
             Update or delete
           </NavLink>
-          <NavLink className="admin-link" to={"./user-gestion"}>
+          <NavLink className="standard-button" to={"./user-gestion"}>
             User gestion
           </NavLink>
         </section>
